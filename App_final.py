7900eef151c2fda5f3be94adefdb58e6caeecf6c
senakshikrishnamurthy <mksenaakshi@gmail.com>
2025-12-03 22:24:11 -0500

    corrected_app_final

--- App_final.py
+++ App_final.py
@@ -224,13 +224,15 @@ def serve_model(filename):
 # Auto-open browser
 import webbrowser
 from threading import Timer
+import os   # 👈 add this
 
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000/")
 
 if __name__ == "__main__":
-    # Open browser after 1.5 seconds
-    Timer(1.5, open_browser).start()
+    # Only run this in the reloader child process
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        Timer(1.5, open_browser).start()
 
-    # Start Flask
     app.run(debug=True)
+
